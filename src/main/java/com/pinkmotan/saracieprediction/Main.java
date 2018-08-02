71b98478ff30bb9e3427f1284fe8fd9db7f9ecc9
Alina Gore <agore@inther.local>
2018-08-02 14:43:43 +0300

    Added UJMP dependencies and resource files

---
+++ src/main/java/com/pinkmotan/saracieprediction/Main.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pinkmotan.saracieprediction;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.ujmp.core.Matrix;
+
+/**
+ *
+ * @author agore
+ */
+public class Main {
+
+  public static void main(String[] args) {
+
+    File file = new File(".\\src\\main\\resources\\train.csv");
+    try {
+      Matrix result = Matrix.Factory.importFrom().file(file).asDenseCSV();
+      System.out.println("Columns: " + result.getDimensionCount());
+      System.out.println("First label: " + result.getColumnLabel(0));
+//      result.showGUI();
+
+    } catch (IOException ex) {
+      Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+    }
+  }
+}
