a542260f8562a58fb16a1dfb21d0a7fc32142b57
Alina Gore <agore@inther.local>
2018-08-20 13:35:51 +0300

    Fixed path

--- src/main/java/edu/pinkmotan/saracieprediction/Main.java
+++ src/main/java/edu/pinkmotan/saracieprediction/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.pinkmotan.saracieprediction;
+package edu.pinkmotan.saracieprediction;
 
 import java.io.File;
 import java.io.IOException;
