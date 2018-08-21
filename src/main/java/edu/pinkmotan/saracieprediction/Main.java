c26a07555cf24670cc3bf1c757a52bb3a84c45bf
agore <agore@LT-AGORE.inther.local>
2018-08-21 16:48:01 +0300

    added populateTarget method

--- src/main/java/edu/pinkmotan/saracieprediction/Main.java
+++ src/main/java/edu/pinkmotan/saracieprediction/Main.java
@@ -7,8 +7,12 @@ package edu.pinkmotan.saracieprediction;
 
 import edu.pinkmotan.saracieprediction.household.Person;
 import edu.pinkmotan.saracieprediction.household.PersonHelper;
+import edu.pinkmotan.saracieprediction.math.Gradient;
+import edu.pinkmotan.saracieprediction.math.Matrix;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
@@ -17,8 +21,25 @@ import java.util.List;
 public class Main {
 
   public static void main(String[] args) throws IOException {
-    List<Person> persons = PersonHelper.readPersons("src\\main\\resources\\train.csv");
+    List<Person> trainPersons = PersonHelper.readPersons("src\\main\\resources\\train.csv");
+    List<Person> testPersons = PersonHelper.readPersons("src\\main\\resources\\test.csv");
     
+    List<Person> persons = new ArrayList<>();
+    persons.addAll(trainPersons);
+    persons.addAll(testPersons);
     PersonHelper.populateResults(persons, "src\\main\\resources\\results.csv");
   }
+  
+  private static void populateTarget(List<Person> persons, float f, int i) {
+        List<Person> personsWithTarget = persons.stream().filter(p -> !p.isWithoutTarget()).collect(Collectors.toList());
+        List<Person> personsWithoutTarget = persons.stream().filter(Person::isWithoutTarget).collect(Collectors.toList());
+
+        Matrix m = Matrix.createMatrix(personsWithTarget);
+        double[] theta = Gradient.calculateLogisticTheta(m, f, i);
+
+        personsWithoutTarget.forEach(p -> {
+            double target = Math.round(Gradient.sigmoid(Gradient.calculateCost(theta, p)));
+            p.setTarget((int) Math.round(target));
+        });
+    }
 }
